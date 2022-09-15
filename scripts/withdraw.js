37f20400ff27f0b21825b1fcfd7d929e974a3935
nikos papadakis <npapadakis@gmail.com>
2022-09-16 00:08:19 +0300

    initial commit

---
+++ scripts/withdraw.js
@@ -0,0 +1,18 @@
+const { getNamedAccounts, ethers } = require("hardhat")
+async function main() {
+    const { deployer } = await getNamedAccounts()
+    const fundMe = await ethers.getContract("FundMe", deployer)
+    console.log("Withdrawing from contract...")
+
+    const transactioResponse = await fundMe.withdraw()
+
+    await transactioResponse.wait(1)
+    console.log("Got it back!")
+}
+
+main()
+    .then(() => process.exit(0))
+    .catch((error) => {
+        console.error(error)
+        process.exit(1)
+    })
